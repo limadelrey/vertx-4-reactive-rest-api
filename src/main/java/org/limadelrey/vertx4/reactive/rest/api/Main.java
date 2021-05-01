10044ed05fbbca4248eeb99fbafeea444843e16c
limadelrey <jfmpeixotolima@gmail.com>
2021-05-01 21:20:21 +0100

    Enable JVM and uptime metrics

--- src/main/java/org/limadelrey/vertx4/reactive/rest/api/Main.java
+++ src/main/java/org/limadelrey/vertx4/reactive/rest/api/Main.java
@@ -1,8 +1,10 @@
 package org.limadelrey.vertx4.reactive.rest.api;
 
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.micrometer.MetricsNaming;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxPrometheusOptions;
 import org.limadelrey.vertx4.reactive.rest.api.verticle.MainVerticle;
@@ -12,10 +14,14 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
 
+        PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        new UptimeMetrics().bindTo(registry);
+
         final Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(
                 new MicrometerMetricsOptions()
                         .setPrometheusOptions(new VertxPrometheusOptions().setEnabled(true))
-                        .setMetricsNaming(MetricsNaming.v3Names())
+                        .setJvmMetricsEnabled(true)
+                        .setMicrometerRegistry(registry)
                         .setEnabled(true)));
 
         vertx.deployVerticle(MainVerticle.class.getName())
